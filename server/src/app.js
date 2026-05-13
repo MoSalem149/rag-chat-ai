2fa8465ac76f92972f7bfe2b0c71d3b6d7018dc2
MoSalem149 <mohamed.salem.dev.official@gmail.com>
2026-05-13 23:43:03 +0300

    refactor server initialization and MongoDB connection handling

--- server/src/app.js
+++ server/src/app.js
@@ -8,23 +8,28 @@ import chatRoutes from "./routes/chat.routes.js";
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Mongo Connected"))
-  .catch((err) => console.log(err));
-
-app.use("/api/chat", chatRoutes);
-
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on ${PORT}`);
-});
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Mongo Connected");
+
+    app.use("/api/chat", chatRoutes);
+
+    app.get("/", (req, res) => {
+      res.send("API Running");
+    });
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  } catch (err) {
+    console.error("DB Connection Error:", err);
+  }
+}
+
+startServer();
